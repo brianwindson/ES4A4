7b4abbf2296e6f28980a00d29cc26f01167d8e46
Brian Windson <60999180+brianwindson@users.noreply.github.com>
2021-09-23 16:47:11 -0300

    Interface básica da calculadora

---
+++ Calculadora.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int num1, num2;
+
+int main(){
+    int opc;
+
+    do{
+        	printf("\n\n OPCOES: \n");
+			printf("\n 1 - Soma  ");
+			printf("\n 2 - Subtracao ");
+			printf("\n 3 - Multiplicacao ");
+			printf("\n 4 - Divisao  ");
+            printf("\n 5 - Operacao aleatoria ");
+            printf("\n 6 - Sair");
+			printf("\n opcao desejada:");
+			scanf("%d", &opc);
+			switch (opc){
+                case 1:
+                    break;
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+                
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+            }
+    }
+    while ( opc != 6);
+}
